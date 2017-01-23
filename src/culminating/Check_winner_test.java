d032d4ce42c9af4ca943bbc4ad7a227eb78e52b5
yummy <yummy@192.168.64.192>
2017-01-23 10:23:22 -0500

    Add Check_Winner_Test class

--- src/culminating/Check_winner_test.java
+++ src/culminating/Check_winner_test.java
@@ -1,7 +1,49 @@
 package culminating;
 
+import java.util.Scanner;
+
 public class Check_winner_test {
+
+	public static int[][] grid;
+	public static Scanner scan = new Scanner(System.in);
+
 	public static void main(String[] args) {
+		boolean userContinue = true;
 		
+		do {
+			System.out.println("\n==================\n"
+					+ "PVP GAME SELECTED!\n"
+					+ "==================\n\n"
+					+ "Please enter the desired grid size (MAX: 10) :");
+			int gridSize = Integer.parseInt(scan.nextLine());
+			
+			doMove(gridSize);
+			checkWinner();
+			
+		} while(userContinue);
+		
+	}
+
+	/**
+	 * Allows the current player to make a move
+	 * @param player - current player number making the move.
+	 **/
+	public static void doMove(int player) {
+		System.out.println("Player " + player + "'s move:"
+				+ "\nPlease choose a column to place your piece");
+	}
+	
+	public static int[][] placePiece(int gridSize) {
+		int[][] updatedGrid = new int[gridSize][gridSize];
+		
+		
+		return updatedGrid;
+	}
+
+	/*
+	 * Checks to see if the user won after their move
+	 */
+	public static void checkWinner() {
+
 	}
 }
